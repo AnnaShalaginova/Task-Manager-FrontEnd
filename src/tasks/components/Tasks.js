8de5ad39a917822730bd8e9cf5b89adf96a4f07f
AnnaShalaginova <anya.shalaginova@gmail.com>
2019-07-24 15:15:47 -0400

    Updates to tasks

--- src/tasks/components/Tasks.js
+++ src/tasks/components/Tasks.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import apiUrl from '../../apiConfig'
 import ListGroup from 'react-bootstrap/ListGroup'
 import { Link } from 'react-router-dom'
-import Button from 'react-bootstrap/Button'
+// import Button from 'react-bootstrap/Button'
 import Layout from '../../shared/Layout'
 
 class Tasks extends Component {
@@ -15,6 +15,25 @@ class Tasks extends Component {
       sorted: false
     }
   }
+  // {
+  //   user ? (
+  //     <div>
+  //       <Button variant="outline-secondary" href="#createbook" className="mr-2">
+  //         <i className="icofont-ui-add"></i>
+  //       </Button>
+  //       <Button variant={filtered ? 'outline-primary' : 'outline-secondary'} onClick={this.handleFilter} className="mr-2">
+  //         <i className="icofont-filter"></i>
+  //       </Button>
+  //       <Button variant={sorted ? 'outline-primary' : 'outline-secondary'} onClick={this.handleSort}>
+  //         <i className="icofont-sort"></i>
+  //       </Button>
+  //     </div>
+  //   ) : (
+  //     <Button variant={sorted ? 'outline-primary' : 'outline-secondary'} onClick={this.handleSort}>
+  //       <i className="icofont-sort"></i>
+  //     </Button>
+  //   )
+  // }
 
   componentDidMount () {
     axios(`${apiUrl}/tasks`)
@@ -33,7 +52,7 @@ class Tasks extends Component {
   }
 
   render () {
-    const { tasks, filtered, sorted } = this.state
+    const { tasks, filtered } = this.state
     const { user } = this.props
 
     const taskArray = tasks.map(task => (
@@ -52,25 +71,7 @@ class Tasks extends Component {
       <Layout md="8" lg="6">
         <div className="d-flex justify-content-between mb-2">
           <h3>Tasks</h3>
-          {
-            user ? (
-              <div>
-                <Button variant="outline-secondary" href="#createbook" className="mr-2">
-                  <i className="icofont-ui-add"></i>
-                </Button>
-                <Button variant={filtered ? 'outline-primary' : 'outline-secondary'} onClick={this.handleFilter} className="mr-2">
-                  <i className="icofont-filter"></i>
-                </Button>
-                <Button variant={sorted ? 'outline-primary' : 'outline-secondary'} onClick={this.handleSort}>
-                  <i className="icofont-sort"></i>
-                </Button>
-              </div>
-            ) : (
-              <Button variant={sorted ? 'outline-primary' : 'outline-secondary'} onClick={this.handleSort}>
-                <i className="icofont-sort"></i>
-              </Button>
-            )
-          }
+
         </div>
         <ListGroup>
           {taskArray}
